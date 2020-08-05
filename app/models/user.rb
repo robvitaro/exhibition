266f4fbd5ce4d4a05b430371c3ac84468dffa4f3
Rob Vitaro <robvitaro@gmail.com>
2020-08-05 16:36:39 -0400

    scoped users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
          :rememberable, :trackable, :validatable, :confirmable, :lockable
 
   has_many :galleries
+
+  scope :confirmed, -> { where.not(confirmed_at: nil) }
+  scope :galleried, -> { joins(:galleries)}
 end
