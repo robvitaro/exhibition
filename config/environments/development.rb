f1b47be44676caf338e03b65f6f15148f02ec693
Rob Vitaro <robvitaro@gmail.com>
2020-08-12 14:29:58 -0400

    don't replace images when adding new ones

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.active_storage.replace_on_assign_to_many = false
 
   # Mailing
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
